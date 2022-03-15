e3032d6883e30bde21f17364d3c57aa8247d3e57
Shadat <shadat.rahman.464@gmail.com>
2022-03-15 13:24:05 +0600

    initial commit

---
+++ lib/presentation/color_manager.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+class ColorManager{
+  static Color primary = HexColor.fromHex("#ed9728");
+  static Color darkGrey = HexColor.fromHex("#525252");
+  static Color grey = HexColor.fromHex("#737477");
+  static Color lightGrey = HexColor.fromHex("#9e9e9e");
+  static Color primaryOpacity70 = HexColor.fromHex("#b3ed9728");
+}
+extension HexColor on Color{
+  static Color fromHex(String hexColorString){
+    hexColorString = hexColorString.replaceAll('#', '');
+    if(hexColorString.length == 6){
+      hexColorString = 'ff' + hexColorString;
+    }
+    return Color(int.parse(hexColorString, radix: 16));
+  }
+}
---
+++ lib/presentation/styles_manager.dart
@@ -0,0 +1,43 @@
+import 'package:advanced_flutter/presentation/font_manager.dart';
+import 'package:flutter/cupertino.dart';
+
+TextStyle _getTextStyle(
+    double fontSize, String fontFamily, FontWeight fontWeight, Color color) {
+  return TextStyle(
+    fontSize: fontSize,
+    fontFamily: fontFamily,
+    color: color,
+    fontWeight: fontWeight,
+  );
+}
+
+//Regular Style
+TextStyle getRegularStyle(
+    {double fontSize = FontSize.s12, required Color color}) {
+  return _getTextStyle(
+      fontSize, FontConstants.fontFamily, FontWeightManager.regular, color);
+}
+//Light Style
+TextStyle getLightStyle(
+    {double fontSize = FontSize.s12, required Color color}) {
+  return _getTextStyle(
+      fontSize, FontConstants.fontFamily, FontWeightManager.light, color);
+}
+//Bold Style
+TextStyle getBoldStyle(
+    {double fontSize = FontSize.s12, required Color color}) {
+  return _getTextStyle(
+      fontSize, FontConstants.fontFamily, FontWeightManager.bold, color);
+}
+//SemiBold Style
+TextStyle getSemiBoldStyle(
+    {double fontSize = FontSize.s12, required Color color}) {
+  return _getTextStyle(
+      fontSize, FontConstants.fontFamily, FontWeightManager.semiBold, color);
+}
+//Medium Style
+TextStyle getMediumStyle(
+    {double fontSize = FontSize.s12, required Color color}) {
+  return _getTextStyle(
+      fontSize, FontConstants.fontFamily, FontWeightManager.medium, color);
+}
