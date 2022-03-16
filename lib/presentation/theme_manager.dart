a86a74ade7fc7104b3447199424cdab3acfde308
Shadat <shadat.rahman.464@gmail.com>
2022-03-16 16:03:24 +0600

    Looking for terminal error(test)

--- lib/presentation/theme_manager.dart
+++ lib/presentation/theme_manager.dart
@@ -74,20 +74,28 @@ ThemeData getApplicationTheme() {
       errorStyle: getRegularStyle(color: ColorManager.error),
       enabledBorder: OutlineInputBorder(
         borderSide: BorderSide(color: ColorManager.grey, width: AppSize.s1_5),
-        borderRadius: const BorderRadius.all(Radius.circular(AppSize.s8)),
+        borderRadius: const BorderRadius.all(
+          Radius.circular(AppSize.s8),
+        ),
       ),
       focusedBorder: OutlineInputBorder(
         borderSide:
             BorderSide(color: ColorManager.primary, width: AppSize.s1_5),
-        borderRadius: const BorderRadius.all(Radius.circular(AppSize.s8)),
+        borderRadius: const BorderRadius.all(
+          Radius.circular(AppSize.s8),
+        ),
       ),
       errorBorder: OutlineInputBorder(
         borderSide: BorderSide(color: ColorManager.error, width: AppSize.s1_5),
-        borderRadius: const BorderRadius.all(Radius.circular(AppSize.s8)),
+        borderRadius: const BorderRadius.all(
+          Radius.circular(AppSize.s8),
+        ),
       ),
       focusedErrorBorder: OutlineInputBorder(
         borderSide: BorderSide(color: ColorManager.error, width: AppSize.s1_5),
-        borderRadius: const BorderRadius.all(Radius.circular(AppSize.s8)),
+        borderRadius: const BorderRadius.all(
+          Radius.circular(AppSize.s8),
+        ),
       ),
     ),
   );
