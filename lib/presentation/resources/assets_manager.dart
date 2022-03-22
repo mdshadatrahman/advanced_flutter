e339cf0ed39083b6a3cd08ac638dcd6f49e42484
mdshadatrahman <shadat.rahman.464@gmail.com>
2022-03-22 22:26:25 +0600

    2

--- lib/presentation/resources/assets_manager.dart
+++ lib/presentation/resources/assets_manager.dart
@@ -1,4 +1,9 @@
 const String IMAGE_PATH = "assets/images";
-class ImageAssets{
+
+class ImageAssets {
   static const String splashLogo = "$IMAGE_PATH/splash_logo.png";
+  static const String onBoardingLogo1 = "$IMAGE_PATH/onboarding_logo1.svg";
+  static const String onBoardingLogo2 = "$IMAGE_PATH/onboarding_logo2.svg";
+  static const String onBoardingLogo3 = "$IMAGE_PATH/onboarding_logo3.svg";
+  static const String onBoardingLogo4 = "$IMAGE_PATH/onboarding_logo4.svg";
 }
