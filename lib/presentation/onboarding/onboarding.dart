07206455f673490b58ef59d2b0f880a535760940
mdshadatrahman <shadat.rahman.464@gmail.com>
2022-03-23 23:47:31 +0600

    4

--- lib/presentation/onboarding/onboarding.dart
+++ lib/presentation/onboarding/onboarding.dart
@@ -1,5 +1,10 @@
+import 'package:advanced_flutter/presentation/resources/assets_manager.dart';
 import 'package:advanced_flutter/presentation/resources/color_manager.dart';
+import 'package:advanced_flutter/presentation/resources/string_manager.dart';
+import 'package:advanced_flutter/presentation/resources/values_manager.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
 class OnBoardingView extends StatefulWidget {
   const OnBoardingView({Key? key}) : super(key: key);
 
@@ -8,13 +13,63 @@ class OnBoardingView extends StatefulWidget {
 }
 
 class _OnBoardingViewState extends State<OnBoardingView> {
+  late final List<SliderObject> _list = _getSliderData();
+  PageController _pageController = PageController(initialPage: 0);
+  int _currentIndex = 0;
+
+  List<SliderObject> _getSliderData() => [
+        SliderObject(
+          AppStrings.onBoardingSubTitle1,
+          AppStrings.onBoardingSubTitle1,
+          ImageAssets.onBoardingLogo1,
+        ),
+        SliderObject(
+          AppStrings.onBoardingSubTitle2,
+          AppStrings.onBoardingSubTitle2,
+          ImageAssets.onBoardingLogo2,
+        ),
+        SliderObject(
+          AppStrings.onBoardingSubTitle3,
+          AppStrings.onBoardingSubTitle3,
+          ImageAssets.onBoardingLogo3,
+        ),
+        SliderObject(
+          AppStrings.onBoardingSubTitle4,
+          AppStrings.onBoardingSubTitle4,
+          ImageAssets.onBoardingLogo4,
+        ),
+      ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: ColorManager.white,
-      body: const Center(
-        child: Text('Welcome'),
+      appBar: AppBar(
+        elevation: AppSize.s1_5,
+        systemOverlayStyle: SystemUiOverlayStyle(
+          statusBarColor: ColorManager.white,
+          statusBarBrightness: Brightness.dark,
+          statusBarIconBrightness: Brightness.dark,
+        ),
       ),
+      body: PageView.builder(
+          itemCount: _list.length,
+          controller: _pageController,
+          onPageChanged: (index) {
+            setState(() {
+              _currentIndex = index;
+            });
+          },
+          itemBuilder: (context, index) {
+            //return onboarding page
+          }),
     );
   }
+}
+
+class SliderObject {
+  String title;
+  String subtitle;
+  String image;
+  SliderObject(this.title, this.subtitle, this.image);
 }
